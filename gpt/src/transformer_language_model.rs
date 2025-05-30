f8590ad562649701bdeb316c242bc8fd4ce08922
Atul Varma <varmaa@gmail.com>
2025-05-29 21:15:55 -0400

    Add position embeddings.

--- gpt/src/transformer_language_model.rs
+++ gpt/src/transformer_language_model.rs
@@ -1,22 +1,33 @@
 use anyhow::Result;
-use candle_core::Tensor;
+use candle_core::{IndexOp, Tensor};
 use candle_nn::{Embedding, Linear, Module, VarBuilder};
+
+use crate::BLOCK_SIZE;
 
 /// Number of dimensions in embedding space.
 const N_EMBED: usize = 32;
 
 pub struct TransformerLanguageModel {
     token_embedding_table: Embedding,
+    position_embedding_table: Embedding,
+    positions: Tensor,
     language_head: Linear,
 }
 
 impl TransformerLanguageModel {
     pub fn new(vb: VarBuilder, vocab_size: usize) -> Result<Self> {
-        let token_embedding_table = candle_nn::embedding(vocab_size, N_EMBED, vb.pp("embedding"))?;
-        let language_head = candle_nn::linear_no_bias(N_EMBED, vocab_size, vb.pp("language_head"))?;
+        let device = vb.device();
+        let token_embedding_table =
+            candle_nn::embedding(vocab_size, N_EMBED, vb.pp("token_embedding_table"))?;
+        let position_embedding_table =
+            candle_nn::embedding(BLOCK_SIZE, N_EMBED, vb.pp("position_embedding_table"))?;
+        let positions = Tensor::arange(0 as u32, BLOCK_SIZE as u32, device)?;
+        let language_head = candle_nn::linear(N_EMBED, vocab_size, vb.pp("language_head"))?;
         // TODO: Add the rest of the modules!
         Ok(Self {
             token_embedding_table,
+            position_embedding_table,
+            positions,
             language_head,
         })
     }
@@ -24,9 +35,15 @@ impl TransformerLanguageModel {
 
 impl Module for TransformerLanguageModel {
     fn forward(&self, xs: &Tensor) -> Result<candle_core::Tensor, candle_core::Error> {
-        let xs = self.token_embedding_table.forward(xs)?;
-        let xs = self.language_head.forward(&xs)?;
+        let time_steps = xs.dims2()?.1;
+        let tok_emb = self.token_embedding_table.forward(xs)?;
+        let pos_emb = self
+            .position_embedding_table
+            .forward(&self.positions.i(0..time_steps)?)?;
+        let logits = self
+            .language_head
+            .forward(&tok_emb.broadcast_add(&pos_emb)?)?;
         // TODO: Implement the rest of this!
-        Ok(xs)
+        Ok(logits)
     }
 }
