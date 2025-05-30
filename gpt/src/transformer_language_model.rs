be1ac46208a835d7fab1d61c170bd588f3a37b41
Atul Varma <varmaa@gmail.com>
2025-05-30 19:59:17 -0400

    Define key and query for self-attention head.

--- gpt/src/transformer_language_model.rs
+++ gpt/src/transformer_language_model.rs
@@ -7,10 +7,15 @@ use crate::BLOCK_SIZE;
 /// Number of dimensions in embedding space.
 const N_EMBED: usize = 32;
 
+/// Number of dimensions in self-attention heads
+const HEAD_SIZE: usize = 16;
+
 pub struct TransformerLanguageModel {
     token_embedding_table: Embedding,
     position_embedding_table: Embedding,
     positions: Tensor,
+    key: Linear,
+    query: Linear,
     language_head: Linear,
 }
 
@@ -22,12 +27,16 @@ impl TransformerLanguageModel {
         let position_embedding_table =
             candle_nn::embedding(BLOCK_SIZE, N_EMBED, vb.pp("position_embedding_table"))?;
         let positions = Tensor::arange(0 as u32, BLOCK_SIZE as u32, device)?;
+        let key = candle_nn::linear_no_bias(N_EMBED, HEAD_SIZE, vb.pp("key"))?;
+        let query = candle_nn::linear_no_bias(N_EMBED, HEAD_SIZE, vb.pp("query"))?;
         let language_head = candle_nn::linear(N_EMBED, vocab_size, vb.pp("language_head"))?;
         // TODO: Add the rest of the modules!
         Ok(Self {
             token_embedding_table,
             position_embedding_table,
             positions,
+            key,
+            query,
             language_head,
         })
     }
@@ -40,10 +49,15 @@ impl Module for TransformerLanguageModel {
         let pos_emb = self
             .position_embedding_table
             .forward(&self.positions.i(0..time_steps)?)?;
-        let logits = self
-            .language_head
-            .forward(&tok_emb.broadcast_add(&pos_emb)?)?;
+        let x = tok_emb.broadcast_add(&pos_emb)?;
+        let k = self.key.forward(&x)?;
+        let q = self.query.forward(&x)?;
+
         // TODO: Implement the rest of this!
+        let _ = k;
+        let _ = q;
+
+        let logits = self.language_head.forward(&x)?;
         Ok(logits)
     }
 }
