21f749a8e028717216887f7b09fd2cf0839d3d8d
Atul Varma <varmaa@gmail.com>
2025-05-31 00:18:47 -0400

    Factor out AttentionHead struct.

--- gpt/src/transformer_language_model.rs
+++ gpt/src/transformer_language_model.rs
@@ -9,16 +9,56 @@ use crate::BLOCK_SIZE;
 /// Number of dimensions in embedding space.
 const N_EMBED: usize = 32;
 
-/// Number of dimensions in self-attention heads
-const HEAD_SIZE: usize = 32;
+struct AttentionHead {
+    key: Linear,
+    query: Linear,
+    value: Linear,
+    head_size: usize,
+}
+
+impl AttentionHead {
+    pub fn new(head_size: usize, vb: VarBuilder) -> Result<Self> {
+        let key = candle_nn::linear_no_bias(N_EMBED, head_size, vb.pp("key"))?;
+        let query = candle_nn::linear_no_bias(N_EMBED, head_size, vb.pp("query"))?;
+        let value = candle_nn::linear_no_bias(N_EMBED, head_size, vb.pp("value"))?;
+        Ok(Self {
+            key,
+            query,
+            value,
+            head_size,
+        })
+    }
+}
+
+impl Module for AttentionHead {
+    fn forward(&self, xs: &Tensor) -> candle_core::Result<Tensor> {
+        let device = self.key.weight().device();
+        let batches = xs.dims3()?.0;
+        let time_steps = xs.dims3()?.1;
+
+        let k = self.key.forward(&xs)?;
+        let q = self.query.forward(&xs)?;
+        let v = self.value.forward(&xs)?;
+
+        let wei = (q.matmul(&k.transpose(1, 2)?)? / (self.head_size as f64).powf(0.5))?;
+        assert_eq!(wei.dims3()?, (batches, time_steps, time_steps));
+        let tril_mask = Tensor::tril2(time_steps, DType::U8, device)?
+            .broadcast_as((batches, time_steps, time_steps))?;
+        let wei = tril_mask.where_cond(
+            &wei,
+            &Tensor::full(f32::NEG_INFINITY, (batches, time_steps, time_steps), device)?,
+        )?;
+        let wei = softmax(&wei, 2)?;
+        let out = wei.matmul(&v)?;
+        Ok(out)
+    }
+}
 
 pub struct TransformerLanguageModel {
     token_embedding_table: Embedding,
     position_embedding_table: Embedding,
     positions: Tensor,
-    key: Linear,
-    query: Linear,
-    value: Linear,
+    sa_head: AttentionHead,
     language_head: Linear,
 }
 
@@ -30,17 +70,13 @@ impl TransformerLanguageModel {
         let position_embedding_table =
             candle_nn::embedding(BLOCK_SIZE, N_EMBED, vb.pp("position_embedding_table"))?;
         let positions = Tensor::arange(0 as u32, BLOCK_SIZE as u32, device)?;
-        let key = candle_nn::linear_no_bias(N_EMBED, HEAD_SIZE, vb.pp("key"))?;
-        let query = candle_nn::linear_no_bias(N_EMBED, HEAD_SIZE, vb.pp("query"))?;
-        let value = candle_nn::linear_no_bias(N_EMBED, HEAD_SIZE, vb.pp("value"))?;
+        let sa_head = AttentionHead::new(N_EMBED, vb.pp("sa_head"))?;
         let language_head = candle_nn::linear(N_EMBED, vocab_size, vb.pp("language_head"))?;
         Ok(Self {
             token_embedding_table,
             position_embedding_table,
             positions,
-            key,
-            query,
-            value,
+            sa_head,
             language_head,
         })
     }
@@ -48,28 +84,13 @@ impl TransformerLanguageModel {
 
 impl Module for TransformerLanguageModel {
     fn forward(&self, xs: &Tensor) -> Result<candle_core::Tensor, candle_core::Error> {
-        let device = self.positions.device();
-        let batches = xs.dims2()?.0;
         let time_steps = xs.dims2()?.1;
         let tok_emb = self.token_embedding_table.forward(xs)?;
         let pos_emb = self
             .position_embedding_table
             .forward(&self.positions.i(0..time_steps)?)?;
         let x = tok_emb.broadcast_add(&pos_emb)?;
-        let k = self.key.forward(&x)?;
-        let q = self.query.forward(&x)?;
-        let v = self.value.forward(&x)?;
-
-        let wei = (q.matmul(&k.transpose(1, 2)?)? / (HEAD_SIZE as f64).powf(0.5))?;
-        assert_eq!(wei.dims3()?, (batches, time_steps, time_steps));
-        let tril_mask = Tensor::tril2(time_steps, DType::U8, device)?
-            .broadcast_as((batches, time_steps, time_steps))?;
-        let wei = tril_mask.where_cond(
-            &wei,
-            &Tensor::full(f32::NEG_INFINITY, (batches, time_steps, time_steps), device)?,
-        )?;
-        let wei = softmax(&wei, 2)?;
-        let out = wei.matmul(&v)?;
+        let out = self.sa_head.forward(&x)?;
         let logits = self.language_head.forward(&out)?;
         Ok(logits)
     }
