1268b092bd7ea066b4ff3d630dcb2797466aaf04
Atul Varma <varmaa@gmail.com>
2025-06-05 01:19:11 -0400

    Use thiserror in web.

--- gpt/web/src/lib.rs
+++ gpt/web/src/lib.rs
@@ -1,6 +1,17 @@
 use wasm_bindgen::prelude::*;
 
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum GptError {
+    #[error("foooooo")]
+    Foo,
+}
+
 #[wasm_bindgen]
-pub fn add(a: u32, b: u32) -> u32 {
-    a + b
+pub fn add(a: u32, b: u32) -> Result<u32, JsError> {
+    if a == 3 {
+        return Err(GptError::Foo.into());
+    }
+    Ok(a + b)
 }
